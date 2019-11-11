53916e378de7023e78fc1ce2242332783b56c066
Juan <juan.isazam@upb.edu.co>
2019-11-11 00:22:07 -0500

    Proyecto base

---
+++ locker_reading/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="locker_reading.MvcApplication" Language="C#" %>
---
+++ locker_reading/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(locker_reading.Startup))]
+namespace locker_reading
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
