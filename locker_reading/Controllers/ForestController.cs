bd3ab7b80bf6dc4ae30dc8df856f665cfba3621f
Julián Ramírez <juan.isazam@upb.edu.co>
2019-11-11 16:52:57 -0500

    Controlador bosque

---
+++ locker_reading/Controllers/ForestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using locker_reading.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace locker_reading.Controllers
+{
+    [Authorize]
+    public class ForestController : Controller
+    {
+        ApplicationDbContext context;
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public ForestController()
+        {
+            context = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        }
+        // GET: Forest
+        public async Task<ActionResult> Index()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            var entradas = context.Lectures.Where(x => x.ApplicationUser.Id.Equals(user.Id)).ToList();
+            if (entradas.Count != 0)
+            {
+                var result = context.Lectures.Where(x => x.ApplicationUser.Id.Equals(user.Id)).Sum(x => x.NumAdvance);
+                ViewBag.Arboles = result / 100;
+            }
+            else
+            {
+                ViewBag.Arboles = 0;
+            }
+
+            return View();
+        }
+    }
+}
