062e06ed37ec7b295b0c3a25d9aa17708b8565c1
Juan Isaza <jisazamp@gmail.com>
2019-11-12 12:10:12 -0500

    Correcciones registro

--- locker_reading/Models/AccountViewModels.cs
+++ locker_reading/Models/AccountViewModels.cs
@@ -72,11 +72,11 @@ namespace locker_reading.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirmar contraseña")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
